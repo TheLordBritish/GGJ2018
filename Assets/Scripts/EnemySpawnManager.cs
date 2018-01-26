5d9efe0fb405760610ce087737a7e964ba684342
Savvy2904 <claireoliver@Admins-MacBook-Pro-3.local>
2018-01-26 23:00:47 +0000

    spawn code

--- Assets/Scripts/EnemySpawnManager.cs
+++ Assets/Scripts/EnemySpawnManager.cs
@@ -4,13 +4,40 @@ using UnityEngine;
 
 public class EnemySpawnManager : MonoBehaviour {
 
+    static int enemyCount;
+    public int maxEnemies;
+    public GameObject enemy;
+    public GameObject target;
+
+    public float timer;
+    private bool hasSpawned;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+    {
+        timer = 3.0f;
+        enemyCount = 0;
+        hasSpawned = false;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+    {
+        if (timer >= 3.0f)
+        {
+            if (enemyCount < maxEnemies)
+            {
+                Instantiate(enemy, transform.position, Quaternion.identity);
+                enemyCount++;
+                hasSpawned = true;
+                timer = 0.0f;
+            }
+        }
+
+        if (hasSpawned)
+            timer += Time.deltaTime;
+
+        else
+            timer = 3.0f;
 	}
 }
