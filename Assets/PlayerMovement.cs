2dac2174d53d72d03c3573f8e7ebe27e6656b2a6
Savvy2904 <claireoliver@Admins-MacBook-Pro-3.local>
2018-01-26 20:53:09 +0000

    player movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed; 
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log(("forward")); 
+            transform.Translate(Vector3.forward * Time.deltaTime * speed); 
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            Debug.Log(("backwards"));
+            transform.Translate(-Vector3.forward * Time.deltaTime * speed);
+        }
+
+        if(Input.GetKey((KeyCode.D)))
+        {
+            Debug.Log(("forward"));
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey((KeyCode.A)))
+        {
+            Debug.Log(("forward"));
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+	}
+}
