0759a4dc2dd4d3aca86274f99b741c380e6c0ebc
Savvy2904 <claireoliver@Admins-MacBook-Pro-3.local>
2018-01-26 21:58:17 +0000

    generator manager created

---
+++ Assets/GeneratorManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratorManager : MonoBehaviour {
+
+    List<GameObject> lights;
+
+    private bool isCharged;
+    private float chargeLevel;
+
+	// Use this for initialization
+	void Start () 
+    {
+        lights = new List<GameObject>(GameObject.FindGameObjectsWithTag("light"));
+
+        isCharged = true;
+        chargeLevel = 10.0f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        chargeLevel -= Time.deltaTime;
+        TurnOffLights();
+	}
+
+    public void TurnOffLights()
+    {
+        if (chargeLevel <= 0.0f)
+        {
+            isCharged = false;
+
+            foreach (GameObject l in lights)
+            {
+                // l.GetComponent(light code here);
+            }
+        }
+    }
+}
