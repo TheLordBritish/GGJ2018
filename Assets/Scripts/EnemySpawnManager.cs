ca8c82eeec38b5b7b2b8e30a51351637a3fa65da
Ben Carter <bencarterdev@outlook.com>
2018-01-28 00:40:42 +0000

    Murder those child-stealing particle bastards.

--- Assets/Scripts/EnemySpawnManager.cs
+++ Assets/Scripts/EnemySpawnManager.cs
@@ -2,38 +2,24 @@
 
 public class EnemySpawnManager : MonoBehaviour
 {
-    static int enemyCount;
-    public int maxEnemies;
     public GameObject enemy;
 
     private float timer;
     private GeneratorManager generator;
 
-	// Use this for initialization
 	void Start () 
     {
         timer = 3.0f;
-        enemyCount = 0;
-
         generator = GameObject.FindGameObjectWithTag("Generator").GetComponent<GeneratorManager>();
 	}
 	
-	// Update is called once per frame
 	void Update () 
     {
         timer -= Time.deltaTime;
-
-        if (!generator.IsPoweredOn)
+        if (!generator.IsPoweredOn && timer <= 0.0f)
         {
-            if (timer <= 0.0f)
-            {
-                if (enemyCount < maxEnemies)
-                {
-                    Instantiate(enemy, transform.position, Quaternion.identity);
-                    enemyCount++;
-                    timer = 3.0f;
-                }
-            }
+            Instantiate(enemy, transform.position, Quaternion.identity);
+            timer = 3.0f;
         }
 	}
 }
---
+++ Assets/Scripts/ParticleController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour
+{
+    private ParticleSystem system;
+
+    void Awake()
+    {
+        system = GetComponent<ParticleSystem>();
+    }
+	
+	void Update ()
+    {
+	    if (!system.IsAlive())
+        {
+            Destroy(gameObject);
+        }
+	}
+}
